354bab126307c918e35a466dc0c5d3fffed2b2bf
JohnDohnut <lgj1089@gmail.com>
2025-01-15 22:31:22 +0900

    prep done

---
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.model.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(true)
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    void testMember() {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+
+}
